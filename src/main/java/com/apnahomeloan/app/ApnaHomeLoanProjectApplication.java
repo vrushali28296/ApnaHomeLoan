e158d06ea0f20474b17ed68e2a341eee63ed5c97
vrushali28296 <vrushpkumbhar@gmail.com>
2022-09-10 13:49:04 +0530

    Project Added to git hub

---
+++ src/main/java/com/apnahomeloan/app/ApnaHomeLoanProjectApplication.java
@@ -0,0 +1,13 @@
+package com.apnahomeloan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApnaHomeLoanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApnaHomeLoanProjectApplication.class, args);
+	}
+
+}
