83fd2a6646497c96cf7d7899b4466162e0bb962a
vrushali28296 <vrushpkumbhar@gmail.com>
2022-09-12 22:32:24 +0530

    checking customer

--- src/main/java/com/apnahomeloan/app/controller/CibilController.java
+++ src/main/java/com/apnahomeloan/app/controller/CibilController.java
@@ -3,10 +3,12 @@ package com.apnahomeloan.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apnahomeloan.app.model.Cibil;
@@ -14,29 +16,30 @@ import com.apnahomeloan.app.serviceinterface.ApplicantServiceI;
 import com.apnahomeloan.app.serviceinterface.CibilServiceInterface;
 
 @RestController
+@CrossOrigin("*")
+//@RequestMapping()
 public class CibilController {
 
 	@Autowired
 	private CibilServiceInterface csi;
-	
+
 	@Autowired
 	private ApplicantServiceI asi;
-	
-	@PostMapping("/Cibil/{cibilScore}/{applicant_id}")
-	public ResponseEntity<Cibil> saveCibil(@PathVariable int cibilScore,@PathVariable int applicant_id ){
-		
-		
-						Cibil cibil=csi.saveCibil(cibilScore,applicant_id);
-									
-			return new ResponseEntity<Cibil>(cibil,HttpStatus.OK);							
-	}
-	@GetMapping("/getCibil")
-	public Integer  getcibil() {
-		
-			Integer cibil=csi.getcibil(); 
-			
-			return cibil;
+
+	@PostMapping("/saveCibil/{cibilScore}/{applicant_id}")
+	public ResponseEntity<Cibil> saveCibil(@PathVariable int cibilScore, @PathVariable int applicant_id) {
+
+		Cibil cibil = csi.saveCibil(cibilScore, applicant_id);
+
+		return new ResponseEntity<Cibil>(cibil, HttpStatus.OK);
 	}
 
-	
+	@GetMapping("/getCibil")
+	public Integer getcibil() {
+
+		Integer cibil = csi.getcibil();
+
+		return cibil;
+	}
+
 }
