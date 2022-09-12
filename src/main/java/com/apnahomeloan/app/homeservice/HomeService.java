26830ef9cdd63b48d04e06e7835b244c8b159cfe
patilgaurav2020 <gauravmpatil75@gmail.com>
2022-09-12 15:30:19 +0530

    update complete

--- src/main/java/com/apnahomeloan/app/homeservice/HomeService.java
+++ src/main/java/com/apnahomeloan/app/homeservice/HomeService.java
@@ -1,8 +1,5 @@
 package com.apnahomeloan.app.homeservice;
 
-<<<<<<< HEAD
-public class HomeService {
-=======
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,15 +10,16 @@ import com.apnahomeloan.app.serviceinterface.ServiceInterFace;
 @Service
 public class HomeService implements ServiceInterFace{
 
-	@Autowired private ApplicantRepository apr;
-	
+	 @Autowired private ApplicantRepository apr;
 	@Override
 	public void saveApplicant(Applicant a) {
-		
 		apr.save(a);
 		
+	}
+
+	
 		
 	}
->>>>>>> ab5a7bd2d1c7aba5fa3194275a7c0e699c55a933
 
-}
+
+
